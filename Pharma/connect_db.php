3d2571f365ff782e449b17b51b3a648ba796aeb5
sooryar08 <sooryar08@gmail.com>
2015-03-29 19:09:43 +0530

    this is my entire folder

---
+++ Pharma/connect_db.php
@@ -0,0 +1,7 @@
+<?php
+
+error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
+mysql_connect('{mysql}{6553}{env-5549706}.{ind-cloud.everdata.com}','root','ZYCsao06685')or die("cannot connect to server");
+mysql_select_db('pharmacy')or die("cannot connect to database");
+
+?>
